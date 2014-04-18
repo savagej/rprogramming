6df8c09a5064a4ed82d34722e46dfef438e07898
savagej <dtjohnnyb@gmail.com>
2014-04-18 12:57:30 -0500

    Finished homework assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## Edited file by savagej
+## These two functions are for calculating the inverse of a matrix and 
+## caching it for later use
 
-## Write a short comment describing this function
+## This function creates 4 functions and returns them in a list 
+## Functions are accessed by assigning the returned list to a variable
+## eg variable <- makeCacheMatrix(myMatrix)
+## variable$set() will then run the set function etc
+## see comments within function for explanations of each one
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        # set is used to set (or reset) the matrix to be used
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        # get returns the matrix currently being used
+        get <- function() x
+        # setinv is for assigning a result to the cache
+        setinv <- function(result) i <<- result
+        # getinv returns the cached result (if it exists)
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function uses the functions from the list of functions 
+## created in makeCacheMatrix to actually solve the inverse of the matrix
+## or retreive it from the cache if it has been cached
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mcm, ...) {
+	    # Check if there is a cached result, return it if there is
+        inv <- mcm$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        
+        # Get the matrix and solve the inverse
+        data <- mcm$get()
+        inv <- solve(data, ...)
+        # Store the result in the cache
+        mcm$setinv(inv)
+        inv
+
+}
+
+## Test function for checking the inverse and the behaviour of makeCacheMatrix
+
+inverseCheck <- function(mcm, ...) {
+	# multiply the matrices together to see if we really have an inverse
+	inv <- mcm$getinv()
+	mat <- mcm$get()
+	if(is.null(inv)) {
+		message("There's no cache yet, calculating inverse")
+    	inv <- solve(mat, ...)
+    	mcm$setinv(inv)
+    }
+    res <- inv %*% mat
+    res
 }
